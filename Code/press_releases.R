44a488e3bbcb6110555845f5d84a4bea1de194f4
jdrouin13 <jeremie.drouin13@gmail.com>
2023-12-01 15:19:06 -0500

    rouler les dictionnaires sur les communiqués de presse

--- Code/press_releases.R
+++ Code/press_releases.R
@@ -20,3 +20,45 @@ df <- clessnverse::get_warehouse_table(
 names(df)
 table(df$political_party)
 table(df$date)
+
+df_press_release <- df %>% 
+  filter(date>= "2022-08-28", date <= "2022-10-03")
+
+########
+####Appliquer les dictionnaires#####
+########
+
+dict_pimp <- utils::read.csv2("/Users/jeremiedrouin/Dropbox/Travail/Universite_Laval/CLESSN/Publications/article_twitter-elxn22/_SharedFolder_article_twitter-elxn22/dictionnaires/dict_final.csv", encoding = "UTF-8")
+
+dict_listFinal <- list()
+for (c in unique(dict_pimp$category)) {
+  dict_listFinal[[c]] <- dict_pimp$item[dict_pimp$category == c]
+  
+}
+
+dictFinal <- quanteda::dictionary(as.list(dict_listFinal))
+
+
+
+qdictFinal <- quanteda::dictionary(as.list(dict_listFinal))
+
+ReleaseDict <- clessnverse::run_dictionary(data = df_press_release,
+                                           text = df_press_release$body,
+                                           dict = qdictFinal)
+
+releases_full <- cbind(df_press_release, ReleaseDict)
+
+
+########Save le RDS#####
+
+saveRDS(releases_full, "_SharedFolder_article_twitter-elxn22/Data/dict_releases.rds")
+  
+  
+  
+  
+  
+  
+  
+  
+  
+

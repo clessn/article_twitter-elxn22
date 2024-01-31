a0b32d4b675e8e702ddf4a3323ecd502ad62c70d
jdrouin13 <jeremie.drouin13@gmail.com>
2024-01-31 16:51:11 -0500

    sample checkup

--- Code/proportions_corpus.R
+++ Code/proportions_corpus.R
@@ -1,4 +1,7 @@
 library(tidyverse)
+library(ggplot2)
+library(clessnverse)
+library(forcats)
 
 
 
@@ -6,6 +9,26 @@ library(tidyverse)
 
 data_media <- readRDS("/Users/jeremiedrouin/Dropbox/Travail/Universite_Laval/CLESSN/Publications/article_twitter-elxn22/_SharedFolder_article_twitter-elxn22/Data/dict_media.rds")
 data_tweets <- readRDS("/Users/jeremiedrouin/Dropbox/Travail/Universite_Laval/CLESSN/Publications/article_twitter-elxn22/_SharedFolder_article_twitter-elxn22/Data/dict_tweets.rds")
+
+
+##### Random checkup pour valider les dicos
+
+datatweetsample <- data_tweets %>% 
+  select("data.text","macroeconomics", "healthcare", "agriculture", "labour", 
+         "immigration", "environment", "energy", "transportation", 
+         "crime", "culture", "aboriginal", "religion", "education", "cost_life",
+         "langue_fran" )
+
+nombre_total_lignes <- nrow(datatweetsample)
+nombre_lignes_selection <- 150
+indices_aleatoires <- sample(1:nombre_total_lignes, nombre_lignes_selection)
+dataset_selectionne <- datatweetsample[indices_aleatoires, ]
+
+dataset_selectionne[1]
+
+
+
+
 
 
 
@@ -36,15 +59,30 @@ row_total_tweets <- sum(tweets_prop)
 percentage_tweets <- tweets_prop %>%
   mutate_all(function(x) (x / row_total_tweets) * 100)
 
+names(percentage_tweets) <- c("Macro-\n economics", "Health-\n care", "Agriculture", "Labour", 
+                              "Immigration", "Environment", "Energy", "Transportation", 
+                              "Crime", "Culture", "Aboriginal \n affairs", "Religion",
+                              "Education", "Cost of \n living", "French \n language")
+
+
 percentage_tweets_long <- tidyr::gather(percentage_tweets, key = "variable", value = "percentage")
 
+percentage_tweets_long <- percentage_tweets_long %>% 
+  mutate_if(is.numeric, round)
+
 # Create a bar plot using ggplot2
-ggplot(percentage_tweets_long, aes(x = variable, y = percentage, fill = variable)) +
+ggplot(percentage_tweets_long, aes(x = reorder(variable, percentage), y= percentage, fill = variable)) +
   geom_bar(stat = "identity", position = "dodge") +
-  labs(title = "Proportion de chaque enjeu, Twitter",
-       x = "Variables",
+  labs(title = "",
+       x = "",
        y = "Percentage") +
-  theme_minimal()
+  geom_text(aes(label=percentage), position=position_dodge(width=0.9), vjust=-0.25, size= 7)+
+  theme_clean_light()+
+  theme(legend.position = "none",
+        axis.text.x= element_text(angle=45, hjust = 1, size=20),
+        axis.title.y = element_text(size=20))
+
+ggsave("_SharedFolder_article_twitter-elxn22/graphs/tweets_prop.png", height= 10, width= 15)
 
 
 
@@ -83,16 +121,29 @@ row_total <- sum(media_prop)
 percentage_media <- media_prop %>%
   mutate_all(function(x) (x / row_total) * 100)
 
+names(percentage_media) <- c("Macro-\n economics", "Health-\n care", "Agriculture", "Labour", 
+                             "Immigration", "Environment", "Energy", "Transportation", 
+                             "Crime", "Culture", "Aboriginal \n affairs", "Religion",
+                             "Education", "Cost of \n living", "French \n language")
+
 percentage_media_long <- tidyr::gather(percentage_media, key = "variable", value = "percentage")
 
-# Create a bar plot using ggplot2
-ggplot(percentage_media_long, aes(x = variable, y = percentage, fill = variable)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  labs(title = "Proportion de chaque enjeu, médias",
-       x = "Variables",
-       y = "Percentage") +
-  theme_minimal()
+percentage_media_long <- percentage_media_long %>% 
+  mutate_if(is.numeric, round)
 
+# Create a bar plot using ggplot2
+ggplot(percentage_media_long, aes(x = reorder(variable, percentage), y= percentage, fill = variable)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "",
+       x = "",
+       y = "Percentage") +
+  geom_text(aes(label=percentage), position=position_dodge(width=0.9), vjust=-0.25, size= 7)+
+  theme_clean_light()+
+  theme(legend.position = "none",
+        axis.text.x= element_text(angle=45, hjust = 1, size=20),
+        axis.title.y = element_text(size=20))
+
+ggsave("_SharedFolder_article_twitter-elxn22/graphs/media_prop.png", height= 10, width= 15)
 
 
 
